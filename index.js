07f4387fd9e296308fbc6b72158b3c80924f6f20
khanh <ltk1909@gmail.com>
2020-05-01 03:18:19 +0700

    2

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+const port = 6600;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on("connection", function(socket){
+    console.log('a user connected');
+    
+    socket.on('new_visitor', user => {
+        console.log('new_visitor', user);
+        socket.user = user
+    })
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    })
+})
+
+http.listen(port, () => {
+    console.log(`listening on :${port}`);
+})
